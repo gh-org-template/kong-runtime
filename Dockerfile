03d0b0d8697465b30f6091461d770dcd782c47cd
Colin Hutchinson <hutchic@users.noreply.github.com>
2024-07-25 20:25:03 +0000

    feat(kong-runtime): setup the kong-runtime build / release (#3)
    * feat(kong-runtime): setup the kong-runtime build / release
    
    * fix(submodule): the checkout task requires a token to clone submodule submodules
    
    * chore: recursive submodule clone not just true

--- Dockerfile
+++ Dockerfile
@@ -4,17 +4,30 @@ ARG DOCKER_REGISTRY=ghcr.io
 ARG DOCKER_IMAGE_NAME
 
 # List out all image permutations to trick dependabot
-FROM --platform=linux/amd64 ghcr.io/gh-org-template/kong-build-images:apk AS x86_64-linux-musl
-FROM --platform=linux/amd64 ghcr.io/gh-org-template/kong-build-images:rpm AS x86_64-linux-gnu
-FROM --platform=linux/arm64 ghcr.io/gh-org-template/kong-build-images:apk AS aarch64-linux-musl
-FROM --platform=linux/arm64 ghcr.io/gh-org-template/kong-build-images:rpm AS aarch64-linux-gnu
+FROM --platform=linux/amd64 ghcr.io/gh-org-template/kong-openssl:1.0.0-linux-musl AS x86_64-linux-musl
+FROM --platform=linux/amd64 ghcr.io/gh-org-template/kong-openssl:1.0.0-linux-gnu AS x86_64-linux-gnu
+FROM --platform=linux/arm64 ghcr.io/gh-org-template/kong-openssl:1.0.0-linux-musl AS aarch64-linux-musl
+FROM --platform=linux/arm64 ghcr.io/gh-org-template/kong-openssl:1.0.0-linux-gnu AS aarch64-linux-gnu
 
 # Run the build script
 FROM $ARCHITECTURE-$OSTYPE AS build
 
-COPY . /src
-RUN /src/build.sh && /src/test.sh
+COPY . /tmp
+WORKDIR /tmp
 
+# Run our predecessor tests
+# Configure, build, and install
+# Run our own tests
+# Re-run our predecessor tests
+ENV DEBUG=0
+RUN /test/*/test.sh && \
+    /tmp/build.sh && \
+    /tmp/test.sh && \
+    /test/*/test.sh
+
+# Test scripts left where downstream images can run them
+COPY test.sh /test/kong-runtime/test.sh
+COPY .env /test/kong-runtime/.env
 
 # Copy the build result to scratch so we can export the result
 FROM scratch AS package
--- test.sh
+++ test.sh
@@ -1,14 +1,41 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-IFS=$'\n\t'
 
 if [ -n "${DEBUG:-}" ]; then
     set -x
 fi
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export $(grep -v '^#' $SCRIPT_DIR/.env)
+
 function test() {
-    ls -lah /tmp/build/out
+    echo '--- testing kong runtime (openresty, luarocks) ---'
+    cp -R /tmp/build/* /
+    mv /tmp/build /tmp/buffer # Check we didn't link dependencies to `/tmp/build/...`
+
+    ls -la /tmp/buffer/usr/local/openresty/bin/openresty
+    /usr/local/openresty/bin/openresty -v 2>&1 | grep -q ${OPENRESTY_VERSION}
+    /usr/local/openresty/bin/openresty -V 2>&1 | grep -q pcre
+    /usr/local/openresty/bin/openresty -V 2>&1 | grep -q with-pcre=
+    ! ldd /usr/local/openresty/nginx/sbin/nginx | grep pcre
+    /usr/local/openresty/bin/openresty -V 2>&1 | grep -q lua-kong-nginx-module
+    /usr/local/openresty/bin/openresty -V 2>&1 | grep -q lua-resty-lmdb
+    /usr/local/openresty/bin/openresty -V 2>&1 | grep -q lua-resty-events
+    #/usr/local/openresty/bin/resty -e 'print(jit.version)' | grep -q 'LuaJIT[[:space:]][[:digit:]]\+.[[:digit:]]\+.[[:digit:]]\+-[[:digit:]]\{8\}'
+    ls -la /tmp/buffer/usr/local/openresty/nginx/sbin/nginx
+    ls -la /tmp/buffer/usr/local/openresty/bin/resty
+
+    ls -l /usr/local/openresty/lualib/resty/websocket/*.lua
+    grep _VERSION /usr/local/openresty/lualib/resty/websocket/*.lua
+    luarocks --version
+
+    ls -la /tmp/buffer/usr/local/openresty/lualib/libatc_router.so
+    ls -la /tmp/buffer/usr/local/openresty/lualib/resty/router/router.lua
+    #ldd /usr/local/openresty/lualib/libatc_router.so
+
+    mv /tmp/buffer /tmp/build
+    echo '--- tested kong runtime ---'
 }
 
 test
